f9d0616988a58122b1190f3bb10e8b5aa3f74cb1
Mohammad Sadeghi <moh02sadeghi@gmail.com>
2024-10-26 15:39:15 +0330

    feat: improve dark mode toggle button

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import {ThemeContext} from "../context/ThemeContext";
+import {MdModeNight, MdSunny} from "react-icons/md";
 
 const DarkModeToggle = () => {
     const { theme, toggleTheme } = useContext(ThemeContext);
@@ -7,10 +8,11 @@ const DarkModeToggle = () => {
     return (
         <button
             onClick={toggleTheme}
-                className={`btn position-fixed ${theme === 'dark' ? "btn-light" : "btn-dark"}`}
-            style={{right: '4em', bottom: '3em', width: '40px', zIndex: 1000}}
+                className={`btn d-flex justify-content-center align-items-center position-fixed fs-4 p-0 ${theme === 'dark' ? "btn-light" : "btn-dark"}`}
+            style={{right: '4rem', bottom: '3rem', width: '3rem', height: '3rem', zIndex: 1000}}
         >
-            {theme === 'dark' ? '☀' : '☾'}
+
+            {theme === 'dark' ? <MdSunny/> : <MdModeNight/>}
         </button>
     );
 };
